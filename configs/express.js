d477fc224652e9d0beed4b8b99d4e90feccd311d
tamurtaj85 <tamurtaj85@gmail.com>
2021-10-12 15:27:51 +0500

    Final Update

--- configs/express.js
+++ configs/express.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require('../api/routes/');
+require('../api/routes');
 
 app.listen(port);
 
